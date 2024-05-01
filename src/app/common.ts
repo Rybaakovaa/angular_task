17858d021914f484b8b0c8801220738435efa3cc
paa <rybaakovaa@gmail.com>
2024-05-01 22:49:53 +0300

    added dir error

--- src/app/common.ts
+++ src/app/common.ts
@@ -1,7 +1,7 @@
 import {Router} from "@angular/router";
 import {Injectable} from "@angular/core";
-import {Observable} from "rxjs";
 
+declare const bootstrap: any;
 
 @Injectable({
   providedIn: 'root'
